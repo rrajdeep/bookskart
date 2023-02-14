b7ed184620c6db62ea7a2922b9b6d21ded1681ac
Rajdeep <89543670+rrajdeep@users.noreply.github.com>
2023-02-14 13:32:57 +0530

    Create index.js

---
+++ front-end/src/components/footer/index.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./footer.css";
+
+export const Footer = () => {
+    return (
+        <div className="footer-container">
+            <div className="footer-div">
+                <div className="f-main">
+                    <div className="f-div-head">ABOUT</div>
+                    <div className="f-content">Contact Us</div>
+                    <div className="f-content">About Us</div>
+                    <div className="f-content">Careers</div>
+                    <div className="f-content">Press</div>
+                    <div className="f-content">Bookscart Stories</div>
+                    <div className="f-content">Bookscart Wholesale</div>
+                </div>
+                <div className="f-main">
+                    <div className="f-div-head">HELP</div>
+                    <div className="f-content">Payments</div>
+                    <div className="f-content">Shipping</div>
+                    <div className="f-content">Cancellation & Returns</div>
+                    <div className="f-content">FAQ</div>
+                    <div className="f-content">Report Infringement</div>
+                </div>
+                <div className="f-main">
+                    <div className="f-div-head">POLICY</div>
+                    <div className="f-content">Return Policy</div>
+                    <div className="f-content">Terms Of Use</div>
+                    <div className="f-content">Security</div>
+                    <div className="f-content">Privacy</div>
+                    <div className="f-content">Sitemap</div>
+                </div>
+                <div className="f-main">
+                    <div className="f-div-head">SOCIAL</div>
+                    <div className="f-content">Facebook</div>
+                    <div className="f-content">Twitter</div>
+                    <div className="f-content">YouTube</div>
+                </div>
+                <div className="f-main">
+                    <div className="f-div-head">Mail Us:</div>
+                    <div className="f-content">
+                        <p>Bookskart Internet Private Limited,</p>
+                        <p>Buildings Alyssa, Begonia &</p>
+                        <p>Clove Embassy Tech Village,</p>
+                        <p>Outer Ring Road, Devarabeesanahalli Village,</p>
+                        <p>Bengaluru, 560103,</p>
+                        <p>Karnataka, India</p>
+                    </div>
+                </div>
+                <div className="f-main">
+                    <div className="f-div-head">Registerd Office:</div>
+                    <div className="f-content">
+                        <p>Bookskart Internet Private Limited, </p>
+                        <p> Buildings Alyssa, Begonia &amp; </p>
+                        <p> Clove Embassy Tech Village, </p>
+                        <p> Outer Ring Road, Devarabeesanahalli Village, </p>
+                        <p> Bengaluru, 560103, </p>
+                        <p> Karnataka, India </p>
+                        <p> Telephone: <a href="tel:18002029898">044-45614700</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
